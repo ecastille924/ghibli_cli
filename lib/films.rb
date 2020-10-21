e81ffac0d2bbcf21c7f611a06793d85a8e65f499
Erick Castille <erick@Ericks-MacBook-Air.local>
2020-10-20 20:54:06 -0400

    created empty array to store all film objects, and a setter/getter

--- lib/films.rb
+++ lib/films.rb
@@ -1,3 +1,15 @@
 class Film
-  #  attr_accessor :title, :description, :release_date, :director, :producer
+    attr_accessor :title, :description, :release_date, :director, :producer, :rotten_tomatoes_score
+
+    @@all = []
+    
+    def initialize 
+        @@all << self
+    end
+
+    def self.all 
+        @@all
+    end
+
+
 end
