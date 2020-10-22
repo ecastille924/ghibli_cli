bbe3a4c137e312f0609988754135f0dcaf5cdd23
Erick Castille <erick@Ericks-MacBook-Air.local>
2020-10-21 20:10:54 -0400

    built some more functionality into class

--- lib/cli.rb
+++ lib/cli.rb
@@ -11,15 +11,20 @@
         puts "Wanna see a full list of Studio Ghibli films?"
         puts "Type 'y' to continue, or 'n' to exit."
 
-        user_input = gets.strip.dowcase 
+        user_input = gets.strip.downcase 
 
         if user_input == "y"
-            puts "Fatastic!"
+            puts "Fatastic! Here's a list of all 20:"
             show_list_of_films
         end
     end
 
     def show_list_of_films
-            
+        Film.all.each do |film|
+            puts film.title
+        #binding.pry
+        end
     end
 end
+
+puts "hey"
